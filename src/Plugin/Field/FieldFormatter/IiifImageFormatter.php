ff240129e53d7beb0e5e822b555259d8e05727fe
Amit Asaravala <amit@returncontrol.com>
2024-11-15 13:16:19 -0800

    3487873: use EventDispatcherInterface to allow compatibility with Drupal 10.3

--- src/Plugin/Field/FieldFormatter/IiifImageFormatter.php
+++ src/Plugin/Field/FieldFormatter/IiifImageFormatter.php
@@ -9,7 +9,7 @@ use Drupal\Core\Field\Plugin\Field\FieldFormatter\StringFormatter;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\iiif_media_source\Iiif\IiifImageUrlParams;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
  * IIIF Image formatter.
@@ -35,7 +35,7 @@ class IiifImageFormatter extends StringFormatter {
   /**
    * The event Dispatcher.
    *
-   * @var \Symfony\Component\EventDispatcher\EventDispatcher
+   * @var \Symfony\Component\EventDispatcher\EventDispatcherInterface
    */
   protected $eventDispatcher;
 
@@ -58,10 +58,10 @@ class IiifImageFormatter extends StringFormatter {
    *   Any third party settings.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
-   * @param \Symfony\Component\EventDispatcher\EventDispatcher $event_dispatcher
+   * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $event_dispatcher
    *   The event Dispatcher.
    */
-  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, EntityTypeManagerInterface $entity_type_manager, EventDispatcher $event_dispatcher) {
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, EntityTypeManagerInterface $entity_type_manager, EventDispatcherInterface $event_dispatcher) {
     parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings, $entity_type_manager);
 
     $this->eventDispatcher = $event_dispatcher;
